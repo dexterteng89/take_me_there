60daafcaad25d8e607da4851b435147c6fdce371
Nathan Levine <Nathan@Nathans-MacBook-Pro.local>
2013-02-25 18:59:01 -0500

    Second commit: Adding Project for the first time; with photo class

---
+++ TakeMeThere/Photo.h
@@ -0,0 +1,17 @@
+//
+//  Photo.h
+//  TakeMeThere
+//
+//  Created by Nathan Levine on 2/25/13.
+//  Copyright (c) 2013 Nathan Levine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+@property (strong, nonatomic) NSDictionary* myPhoto;
+
+- (NSString *)getPhotoURLBySizeSuffix:(char)sizeSuffix;
+- (UIImage *)getPhotoImage;
+
+@end
---
+++ TakeMeThere/Photo.m
@@ -0,0 +1,27 @@
+//
+//  Photo.m
+//  TakeMeThere
+//
+//  Created by Nathan Levine on 2/25/13.
+//  Copyright (c) 2013 Nathan Levine. All rights reserved.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+@synthesize myPhoto;
+
+- (NSString *)getPhotoURLBySizeSuffix:(char)sizeSuffix
+{
+    NSString* myURL = [NSString stringWithFormat:@"http://farm%@.staticflickr.com/%@/%@_%@_%@.jpg", [self.myPhoto valueForKey:@"farm"], [self.myPhoto valueForKey:@"server"], [self.myPhoto valueForKey:@"id"], [self.myPhoto valueForKey:@"secret"], sizeSuffix];
+    return myURL;
+}
+
+
+- (UIImage *)getPhotoThumbnail
+{
+    UIImage *myImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[self getPhotoURLBySizeSuffix:'t']]]];
+    return myImage;
+}
+
+@end
