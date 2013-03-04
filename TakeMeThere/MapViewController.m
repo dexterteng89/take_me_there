eb536d5c8ba57babc0ad0be72c329d4023fe251e
Nathan Levine <Nathan@Nathans-MacBook-Pro.local>
2013-03-04 12:29:03 -0500

    deleting yelpkit thing

--- TakeMeThere/MapViewController.m
+++ TakeMeThere/MapViewController.m
@@ -58,50 +58,67 @@
     pictureAnnotation.subtitle = @":)";
     pictureAnnotation.coordinate = picCoordinate;
     
+    // add circle using our location as its center and half a kilometer as the radius
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:userLocation.coordinate radius:500];
+    //add circle to map
+    [myMapView addOverlay:circle];
+    //this calls the mapview overlay method below
+    
+    
     [self setsUserLocation];
     
     [myMapView setRegion:myRegion];
     [myMapView addAnnotation:pictureAnnotation];
 }
 
-//- (void)setsUserLocation
-//{
-//    userAnnotation = [[Annotation alloc] init];
-//    userAnnotation.coordinate = userLocation.coordinate;
-//    userAnnotation.title = @"My current Location";
-//    
-//    [myMapView addAnnotation:userAnnotation] ;
-//}
+- (void)setsUserLocation
+{
+    userAnnotation = [[Annotation alloc] init];
+    userAnnotation.coordinate = userLocation.coordinate;
+    userAnnotation.title = @"My current Location";
+    
+    [myMapView addAnnotation:userAnnotation] ;
+}
 
+- (MKOverlayView *)mapView:(MKMapView *)mapView
+            viewForOverlay:(id<MKOverlay>)overlay
+{
+    //add the circleview
+    MKCircleView *view = [[MKCircleView alloc] initWithCircle:overlay];
+    view.fillColor = [[UIColor cyanColor] colorWithAlphaComponent:.3];
+    view.strokeColor = [[UIColor cyanColor] colorWithAlphaComponent:.9];
+    view.lineWidth = 1;
+    return view;
+}
 
-//-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
-//{
-//    MKPinAnnotationView *myPinView =(MKPinAnnotationView *) [mapView dequeueReusableAnnotationViewWithIdentifier:@"ATagPin"];
-//    
-//    if (myPinView == nil)
-//    {
-//        myPinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"ATagPin"];
-//    }
-//    
-//    if ([annotation.title isEqual: @"My current Location"]) {
-//        myPinView.pinColor = MKPinAnnotationColorPurple;
-//        myPinView.canShowCallout = YES;
-//        //annotationView.image = [UIImage imageNamed:@"images.png"];
-//        return myPinView;
-//    } else {
-//        myPinView.pinColor = MKPinAnnotationColorGreen;
-//        myPinView.canShowCallout = YES;
-//        //annotationView.image = [UIImage imageNamed:@"images.png"];
-//        return myPinView;
-//    }
-//
-//    
-//}
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    MKPinAnnotationView *myPinView =(MKPinAnnotationView *) [mapView dequeueReusableAnnotationViewWithIdentifier:@"ATagPin"];
+    
+    if (myPinView == nil)
+    {
+        myPinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"ATagPin"];
+    }
+    
+    if ([annotation.title isEqual: @"My current Location"]) {
+        myPinView.pinColor = MKPinAnnotationColorPurple;
+        myPinView.canShowCallout = YES;
+        //annotationView.image = [UIImage imageNamed:@"images.png"];
+        return myPinView;
+    } else {
+        myPinView.pinColor = MKPinAnnotationColorGreen;
+        myPinView.canShowCallout = YES;
+        //annotationView.image = [UIImage imageNamed:@"images.png"];
+        return myPinView;
+    }
 
-//-(void) showDetail
-//{
-//
-//}
+    
+}
+
+-(void) showDetail
+{
+
+}
 
 - (void)didReceiveMemoryWarning
 {
