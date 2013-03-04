9a6726a9de83d061f4fe70ef346f4e2b31e499fd
Nathan Levine <Nathan@Nathans-MacBook-Pro.local>
2013-03-04 14:00:25 -0500

    added circleview and yelpkit third party framework

---
+++ TakeMeThere/MMYelpKitFramework.h
@@ -0,0 +1,15 @@
+//
+//  MMYelpKitFramework.h
+//  TakeMeThere
+//
+//  Created by Nathan Levine on 3/4/13.
+//  Copyright (c) 2013 Nathan Levine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <YelpKit/YelpKit.h>
+#import <GHKit/GHKit.h>
+
+@interface MMYelpKitFramework : NSObject
+
+@end
---
+++ TakeMeThere/MMYelpKitFramework.m
@@ -0,0 +1,13 @@
+//
+//  MMYelpKitFramework.m
+//  TakeMeThere
+//
+//  Created by Nathan Levine on 3/4/13.
+//  Copyright (c) 2013 Nathan Levine. All rights reserved.
+//
+
+#import "MMYelpKitFramework.h"
+
+@implementation MMYelpKitFramework
+
+@end
